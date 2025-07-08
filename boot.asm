9bcb2a460da53046ab478cb8326ce9d9a81033c2
V1SHAL421 <vishalmkumar3@gmail.com>
2025-07-08 21:24:47 +0100

    feat: implement print, movecursor functions

--- boot.asm
+++ boot.asm
@@ -15,6 +15,10 @@
 ;; and then use call to save the current program counter to the stack, and perform an unconditional
 ;; jump to the given label.
 
+;; The stack pointer (sp) is a 16-bit register that points to the top of the stack.
+;; The base pointer (bp) is a 16-bit register used to reference function params and local vars in the current stack frame.
+;; The stack frame is the area of the stack used by a function. It contains the function params, local vars and return address.
+
 ;;; register ah = function code
 ;;; register al = number of lines to scroll (0 = clear)
 ;;; register bh = colour attribute
@@ -33,3 +37,72 @@
         mov dh, 0x18 ; 18h = 24 rows of chars (lower-right corner row = 0x18 = 24)
         mov dl, 0x4f ; 4fh = 79 cols of chars (bottom-right corner column of window to clear)
         int 0x10 ; calls video interrupt (int 0x10 controls the video output in real mode)
+
+        popa ; pops all the general-purpose registers from the stack in a specific order
+        mov sp, bp ; restores the stack pointer from the base pointer
+        pop bp ; restores the base pointer from the stack
+        ret ; returns control to the calling function by having the CPU jump to the address stored in the stack
+
+    movecursor: ; moving the cursor to an arbitrary (row, col) position on the screen
+        push bp
+        mov bp, sp
+        pusha
+
+        mov dx, [bp+4] ; get the argument from the stack (2 bytes for bp on the stack and 2 bytes for argument on the stack)
+        mov ah, 0x02 ; set the cursor position
+        mov bh, 0x00 ; page 0
+        int 0x10
+
+        popa
+        mov sp, bp
+        pop bp
+        ret
+
+    print:
+        push bp
+        mov bp, sp
+        pusha
+        mov si, [bp+4] ; grab the pointer to the data
+        mov bh, 0x00 ; page number -> 0
+        mov ah, 0x0E ; print character
+
+
+    .char:
+        mov al, [si] ; get the current char form pointer position
+        add si, 1 ; keep incrementing si until we see a null char
+        or al, 0
+        je .return ; end if the string is done
+        int 0x10 ; print the character if we're not done
+        jmp .char ; keep looping
+
+    .return:
+        popa
+        mov sp, bp
+        pop bp
+        ret
+
+    msg:    db "Oh boy do I sure love assembly!", 0
+
+    times 510-($-$$) db 0
+    dw 0xAA55
+
+bits 16
+
+mov ax, 0x07C0
+mov ds, ax
+mov ax, 0x07E0
+mov ss, ax
+mov sp, 0x2000
+
+call clearscreen
+
+push 0x0000
+call movecursor
+add sp, 2
+
+push msg
+call print
+add sp, 2
+
+cli
+hlt
